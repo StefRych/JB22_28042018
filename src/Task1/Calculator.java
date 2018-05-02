9c2134d5132e309a6e23fe12dfba97169911296f
StefRych <stefekr@outlook.com>
2018-05-02 23:34:07 +0200

    Lambda,Strumienie

--- src/Task1/Calculator.java
+++ src/Task1/Calculator.java
@@ -1,19 +1,38 @@
 package Task1;
+//Stwórz za pomocą lambdy kalkulator, powinno być możliwe następujące wywołanie:
+//public static void main(String... args)
+// {
+// Calculator myApp = new Calculator();
+// IntegerMath addition = (a, b) -> a + b;
+// IntegerMath subtraction = (a, b) -> a - b;
+// System.out.println("40 + 2 = " + myApp.operateBinary(40, 2, addition));
+// System.out.println("20 10 = " + myApp.operateBinary(20, 10, subtraction));
+// }
+
+import java.util.function.IntBinaryOperator;
 
 public class Calculator {
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
 
-        IntegerMath add = (a,b) -> a + b;
-        IntegerMath sub = (a,b) -> a - b;
+//        IntegerMath add = (a,b) -> a + b;
+//        IntegerMath sub = (a,b) -> a - b;
+//        IntegerMath div = (a,b) -> a/b;
+//
+//        System.out.println(calculator.operateBinary(5,6,add));
+//        System.out.println(calculator.operateBinary(5,6,sub));
+//        System.out.println(calculator.operateBinary(5,6,div));
 
-        System.out.println(calculator.operateBinary(5,6,add));
-        System.out.println(calculator.operateBinary(5,6,sub));
+        System.out.println(calculator.operateBinary(5,6,(a,b)->a-b));
+        System.out.println(calculator.operateBinary(5,6,(a,b)->a+b));
 
     }
 
-    private int operateBinary(int a, int b, IntegerMath integerMath){
-        return integerMath.operate(a, b);
+//    private int operateBinary(int a, int b, IntegerMath integerMath){
+//        return integerMath.operate(a, b);
+//    }
+    private int operateBinary(int a, int b, IntBinaryOperator integerMath){
+        return integerMath.applyAsInt(a,b);
     }
 }
--- src/Task1/IntegerMath.java
+++ src/Task1/IntegerMath.java
@@ -1,6 +1,6 @@
 package Task1;
 
-public interface IntegerMath {
-
-    int operate(int a, int b);
-}
+//public interface IntegerMath {
+//
+//    int operate(int a, int b);
+//}
